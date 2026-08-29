f2ec8411b725535e4aa5e382b71aa52a024136a4
Ricas13 <56263921+Ricas13@users.noreply.github.com>
2026-08-29 23:50:55 +0100

    test: alias Illuminate Capsule into WHMCS database namespace

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,4 +6,14 @@ if (!defined('WHMCS')) {
     define('WHMCS', true);
 }
 
+$composer = dirname(__DIR__) . '/vendor/autoload.php';
+if (is_file($composer)) {
+    require_once $composer;
+}
+
+if (!class_exists('WHMCS\\Database\\Capsule', false)
+    && class_exists('Illuminate\\Database\\Capsule\\Manager')) {
+    class_alias('Illuminate\\Database\\Capsule\\Manager', 'WHMCS\\Database\\Capsule');
+}
+
 require_once dirname(__DIR__) . '/modules/addons/captainfin/lib/autoload.php';
