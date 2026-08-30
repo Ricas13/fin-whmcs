61fba2140fd428bf280c3d3d145e2f3f313871fc
Ricas13 <56263921+Ricas13@users.noreply.github.com>
2026-08-30 01:20:40 +0100

    Add Emby policy contract tests

---
+++ tests/Emby/PolicyBuilderTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CaptainFin\Whmcs\Tests\Emby;
+
+use CaptainFin\Whmcs\Integrations\Emby\PolicyBuilder;
+use PHPUnit\Framework\TestCase;
+
+final class PolicyBuilderTest extends TestCase
+{
+    public function testActivePolicyDefaultsRemainRestrictive(): void
+    {
+        $policy = PolicyBuilder::active([], true, []);
+
+        self::assertFalse($policy['IsAdministrator']);
+        self::assertFalse($policy['IsDisabled']);
+        self::assertTrue($policy['EnableMediaPlayback']);
+        self::assertTrue($policy['EnableAllFolders']);
+        self::assertFalse($policy['EnableRemoteAccess']);
+        self::assertFalse($policy['EnableContentDownloading']);
+        self::assertFalse($policy['EnableVideoPlaybackTranscoding']);
+        self::assertFalse($policy['EnableAudioPlaybackTranscoding']);
+        self::assertFalse($policy['EnablePlaybackRemuxing']);
+        self::assertFalse($policy['EnableLiveTvAccess']);
+        self::assertFalse($policy['EnableLiveTvManagement']);
+        self::assertTrue($policy['EnableSubtitleManagement']);
+        self::assertSame(0, $policy['SimultaneousStreamLimit']);
+        self::assertArrayNotHasKey('AuthenticationProviderId', $policy);
+        self::assertArrayNotHasKey('SyncPlayAccess', $policy);
+    }
+
+    public function testConfiguredTechnicalOptionsAndNativeStreamLimitAreApplied(): void
+    {
+        $params = [
+            'configoption4' => '3',
+            'configoption13' => 'on',
+            'configoption14' => 'on',
+            'configoption15' => 'on',
+            'configoption16' => 'on',
+            'configoption17' => 'on',
+            'configoption18' => 'on',
+            'configoption19' => 'on',
+            'configoption20' => 'off',
+        ];
+
+        $policy = PolicyBuilder::active($params, false, ['library-1']);
+
+        self::assertFalse($policy['EnableAllFolders']);
+        self::assertSame(['library-1'], $policy['EnabledFolders']);
+        self::assertTrue($policy['EnableContentDownloading']);
+        self::assertTrue($policy['EnableVideoPlaybackTranscoding']);
+        self::assertTrue($policy['EnableAudioPlaybackTranscoding']);
+        self::assertTrue($policy['EnablePlaybackRemuxing']);
+        self::assertTrue($policy['EnableLiveTvAccess']);
+        self::assertTrue($policy['EnableLiveTvManagement']);
+        self::assertTrue($policy['EnableRemoteAccess']);
+        self::assertFalse($policy['EnableSubtitleManagement']);
+        self::assertSame(3, $policy['SimultaneousStreamLimit']);
+    }
+
+    public function testDisabledPolicyRemovesPlaybackAndFolderAccess(): void
+    {
+        $policy = PolicyBuilder::disabled();
+
+        self::assertTrue($policy['IsDisabled']);
+        self::assertFalse($policy['EnableAllDevices']);
+        self::assertFalse($policy['EnableAllFolders']);
+        self::assertSame([], $policy['EnabledFolders']);
+        self::assertFalse($policy['EnableMediaPlayback']);
+        self::assertFalse($policy['EnableRemoteAccess']);
+        self::assertFalse($policy['EnableVideoPlaybackTranscoding']);
+        self::assertFalse($policy['EnableAudioPlaybackTranscoding']);
+        self::assertFalse($policy['EnableContentDownloading']);
+        self::assertSame(0, $policy['SimultaneousStreamLimit']);
+    }
+}
