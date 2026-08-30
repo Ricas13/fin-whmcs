5eba2f2849e5b4faa23a992ac67cd952ed874d19
Ricas13 <56263921+Ricas13@users.noreply.github.com>
2026-08-30 01:13:07 +0100

    Add Emby media server exception

---
+++ modules/addons/captainfin/lib/Integrations/Emby/EmbyException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CaptainFin\Whmcs\Integrations\Emby;
+
+use CaptainFin\Whmcs\Integrations\MediaServer\MediaServerException;
+
+final class EmbyException extends MediaServerException
+{
+}
