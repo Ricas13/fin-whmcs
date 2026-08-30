5ba7eef233a9e4ce069a4819dabf8a62650faf66
Ricas13 <56263921+Ricas13@users.noreply.github.com>
2026-08-30 01:12:28 +0100

    Make Jellyfin errors media-server compatible

--- modules/addons/captainfin/lib/Integrations/Jellyfin/JellyfinException.php
+++ modules/addons/captainfin/lib/Integrations/Jellyfin/JellyfinException.php
@@ -4,42 +4,8 @@ declare(strict_types=1);
 
 namespace CaptainFin\Whmcs\Integrations\Jellyfin;
 
-final class JellyfinException extends \RuntimeException
+use CaptainFin\Whmcs\Integrations\MediaServer\MediaServerException;
+
+final class JellyfinException extends MediaServerException
 {
-    private ?int $statusCode;
-    private bool $retryable;
-    private bool $ambiguous;
-
-    public function __construct(
-        string $message,
-        ?int $statusCode = null,
-        bool $retryable = false,
-        bool $ambiguous = false,
-        ?\Throwable $previous = null
-    ) {
-        parent::__construct($message, 0, $previous);
-        $this->statusCode = $statusCode;
-        $this->retryable = $retryable;
-        $this->ambiguous = $ambiguous;
-    }
-
-    public function statusCode(): ?int
-    {
-        return $this->statusCode;
-    }
-
-    public function isRetryable(): bool
-    {
-        return $this->retryable;
-    }
-
-    public function isAmbiguous(): bool
-    {
-        return $this->ambiguous;
-    }
-
-    public function isNotFound(): bool
-    {
-        return $this->statusCode === 404;
-    }
 }
